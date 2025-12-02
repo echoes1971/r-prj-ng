51d8a222dfd7d53145fc9d49b5ae84e34fc6e0d4
Roberto@Zephyrus <roberto.roccoangeloni@gmail.com>
2025-12-02 16:16:05 +0100

    fe: read environment variables when compiled for production

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './i18n';
 import App from './App';
 import { ThemeProvider } from "./ThemeContext";
+import { app_cfg } from './app.cfg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
+
+// Set page title from runtime config
+document.title = app_cfg.site_title;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
