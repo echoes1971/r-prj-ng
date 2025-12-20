9925f42ec7390a35bde4fd8fe886186e2f6f465c
Roberto@Zephyrus <roberto.roccoangeloni@gmail.com>
2025-12-20 12:52:27 +0100

    refactored

--- fe/src/DBFile.js
+++ fe/src/DBFile.js
@@ -10,6 +10,7 @@ import ObjectLinkSelector from './ObjectLinkSelector';
 import PermissionsEditor from './PermissionsEditor';
 import { formatDescription, formatObjectId } from './sitenavigation_utils';
 import { ThemeContext } from './ThemeContext';
+import { ObjectSearch } from './DBObject';
 
 
 export function FileDownload() {
@@ -473,4 +474,29 @@ export function FileEdit({ data, onSave, onCancel, onDelete, saving, error, dark
             </div>
         </Form>
     );
+}
+
+export function Files() {
+  const { t } = useTranslation();
+  const { dark, themeClass } = useContext(ThemeContext);
+
+  const searchClassname = "DBFile";
+
+  const searchColumns = [
+    { name: t("dbobjects.name") || "Name", attribute: "name", type: "string" },
+    { name: t("dbobjects.description") || "Description", attribute: "description", type: "string" },
+    { name: t("dbobjects.parent") || "Parent", attribute: "father_id", type: "objectLink" },
+  ];
+
+  const resultsColumns = [
+    // { name: t("dbobjects.created") || "Created", attribute: "creator", type: "userLink", hideOnSmall: true },
+    // { name: t("dbobjects.group") || "Group", attribute: "group_id", type: "groupLink", hideOnSmall: true },
+    { name: t("dbobjects.parent") || "Parent", attribute: "father_id", type: "objectLink", hideOnSmall: true },
+    { name: t("files.preview") || "File", attribute: "id", type: "imageView", hideOnSmall: true },
+    { name: t("dbobjects.name") || "Name", attribute: "name", type: "string", hideOnSmall: false },
+    { name: t("dbobjects.description") || "Description", attribute: "description", type: "string", hideOnSmall: true },
+  ];
+  return (
+    <ObjectSearch searchClassname={searchClassname} searchColumns={searchColumns} resultsColumns={resultsColumns} dark={dark} themeClass={themeClass} />
+  );
 }
